606713aed27cfc2b995ef34a0ff0c6a3f78f66d3
Jeferson Lima <jefersonlima@outlook.com>
2024-11-26 06:39:38 -0300

    OrientacaoAObjetos

--- src/OrientacaoAObjetos/AgendaTelefonica.java
+++ src/OrientacaoAObjetos/AgendaTelefonica.java
@@ -4,9 +4,44 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AgendaTelefonica {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Dados dados = new Dados();
 
+        byte opcao;
+
+        do {
+            System.out.print("Digite uma opção: ");
+            System.out.println("\n1. ");
+            System.out.println("2. ");
+            System.out.println("3. ");
+            System.out.println("4. ");
+            System.out.println("5. ");
+
+            opcao = input.nextByte();
+
+            switch (opcao) {
+                case (byte) 1:
+                    dados.addContato(input);
+                    break;
+                case (byte) 2:
+                    dados.deleteContato(input);
+                    break;
+                case (byte) 3:
+                    dados.find(input);
+                    break;
+                case (byte) 4:
+                    dados.listar();
+                    break;
+                case (byte) 5:
+                    break;
+                default:
+                    System.out.println("Operação inválida, tente novamente");
+            }
+        } while (opcao != 5);
+
+        input.close();
 
     }
 }
@@ -38,10 +73,15 @@ class Contato {
 }
 
 class Dados {
+
+    void exibir(String texto) {
+        System.out.println(texto);
+    }
+
     private ArrayList<Contato> listContato;
 
     public Dados() {
-        this.listContato = new ArrayList<> ();
+        this.listContato = new ArrayList<>();
     }
 
     public void addContato(Scanner input) {
@@ -60,10 +100,46 @@ class Dados {
         String nome = input.nextLine();
 
         boolean encontrado = false;
-        for
-    }
-}
+        for (Contato contato : listContato) {
+            if (contato.getNome().equalsIgnoreCase(nome)) {
+                listContato.remove(contato);
+                exibir("Contato removido");
+                encontrado = true;
+                break;
+            }
+        }
 
-void exibir(String texto) {
-    System.out.print(texto);
+        if (!encontrado) {
+            exibir("Contato não encontrado.");
+        }
+    }
+
+    public void find(Scanner input) {
+        exibir("Digite o nome ou telefone do contato: ");
+        String info = input.nextLine();
+
+        boolean encontrado = false;
+
+        for (Contato contato : listContato) {
+            if (contato.getNome().equalsIgnoreCase(info) || contato.getTelefone().equals(info)) {
+                listContato.remove(contato);
+                exibir("Contato: " + contato);
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            exibir("Contato não encontrado.");
+        }
+    }
+
+    public void listar() {
+        if (listContato.isEmpty()) {
+            exibir("Agenda vazia.");
+        } else {
+            exibir("Lista de contatos: ");
+            for (Contato contato : listContato) exibir(contato.toString());
+        }
+    }
 }
